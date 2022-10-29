6113f2ea74e49497592ec56af37c4bf8053be4ce
Tarun Gupta <tarunwebdev@gmail.com>
2022-10-29 21:50:39 +0530

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,16 +16,12 @@ function Home() {
 		>
 			Hey folks 👋
 			<br /> My name is Tarun Gupta. A frontend engineer who craves to learn and
-			develop highly performant isomorphic web applications, study web and
-			functional programming. I keep myself fit by doing crossfit and presently
-			making sincere efforts to learn swimming. I have also started reading
-			books recently.
+			develop highly performant web applications, study web and
+			functional programming. I keep myself fit by doing crossfit and playing sports.
 			<br />
 			<br />
 			<span>
-				Tech Stack:
-				<br />
-				ReactJS | Redux | NextJS | PWA | Javascript | HTML | CSS | NodeJS
+				hey
 			</span>
 		</div>
 	);
